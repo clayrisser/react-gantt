9e8ef7aed7821cb41a959091c9004865407df773
Jam Risser <jamrizzi@gmail.com>
2016-07-26 18:34:53 -0500

    updated timeline accuracy

--- example/src/example.js
+++ example/src/example.js
@@ -7,29 +7,29 @@ var App = React.createClass({
 	render () {
 		var rows = [
 			{
-				title: 'blah blah shirt',
+				title: 'Task 1',
 				startDate: moment().set({date: 15, month: 8, year: 2016}).toDate(),
 				climaxDate: moment().set({date: 20, month: 8, year: 2016}).toDate(),
 				endDate: moment().set({date: 28, month: 8, year: 2016}).toDate(),
-				group: 'popsicle'
+				group: 'myTasks'
 			},
 			{
-				title: 'shoes',
+				title: 'Task 2',
 				startDate: moment().set({date: 8, month: 8, year: 2016}).toDate(),
 				climaxDate: moment().set({date: 13, month: 8, year: 2016}).toDate(),
 				endDate: moment().set({date: 19, month: 8, year: 2016}).toDate(),
-				group: 'popsicle'
+				group: 'myTasks'
 			}
 		];
 		var options = {
 			leftBound: moment().set({hour: 0, date: 1, month: 8, year: 2016}).toDate(),
-			rightBound: moment().set({hour: 23, date: 29, month: 8, year: 2016}).toDate()
+			rightBound: moment().set({hour: 23, date: 29, month: 8, year: 2016}).toDate(),
+			labelWidth: '120px'
 		};
 		var groups = [
 			{
-				id: 'popsicle',
-				title: 'Popsicle',
-				color: 'blue'
+				id: 'myTasks',
+				title: 'My Tasks'
 			}
 		];
 		return (
