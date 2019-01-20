1a67059a9b9069e26e4a7924ceb12b71a0ac2035
Jakub Fridrich <xfridrich@gmail.com>
2019-01-20 21:50:10 +0100

    Change titleStyle from string to object type

--- src/GanttPopup.js
+++ src/GanttPopup.js
@@ -8,7 +8,7 @@ export default class GanttPopup extends Component {
     markerTime: PropTypes.object.isRequired,
     activeStep: PropTypes.object.isRequired,
     title: PropTypes.string.isRequired,
-    titleStyle: PropTypes.string
+    titleStyle: PropTypes.object
   };
   static contextTypes = {
     dateFormat: PropTypes.string.isRequired
